9aacafda699d049482d7bb655afbcc6d9ab8e125
Vasyl Onufriyev <projectclass41@gmail.com>
2019-03-04 17:02:17 -0600

    initial commit

---
+++ BGChanger/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BGChanger
+{
+    class Program
+    {
+        static void Main(string[] args) => BgHandler();
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, String pvParam, uint fWinIni);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+
+        private const int SPI_SETDESKWALLPAPER = 0x14;
+        private const int SPIF_UPDATEINIFILE = 0x1;
+        private const int SPIF_SENDWININICHANGE = 0x2;
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        private static void BgHandler()
+        {
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+            while (true)
+            {
+                CheckActive();
+                Thread.Sleep(500);
+            }
+        }
+
+        public static bool x = false;
+
+        private static void CheckActive()
+        {
+            if (GetActiveWindowTitle() == "Program Manager")
+                DisplayPicture(System.IO.Directory.GetCurrentDirectory() + "\\source.jpg", true);
+            else
+                DisplayPicture(System.IO.Directory.GetCurrentDirectory() + "\\other.jpg", true);
+        }
+
+        private static void DisplayPicture(string file_name, bool update_registry)
+        {
+            try
+            {
+                // If we should update the registry,
+                // set the appropriate flags.
+                uint flags = 0;
+                if (update_registry)
+                    flags = SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE;
+
+                // Set the desktop background to this file.
+                if (!SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                    0, file_name, flags))
+                {
+                    MessageBox.Show("SystemParametersInfo failed.",
+                        "Error", MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error displaying picture " +
+                    file_name + ".\n" + ex.Message,
+                    "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+    }
+}
